fbf03e8cc6d2ab9e9ed8ad6641ca72231b302ac0
joaluba <joa.luberadzka@gmail.com>
2026-08-27 16:47:32 +0200

    Integrated vercel analytics into the project

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@
  */
 
 import VoiceAgent from "./components/VoiceAgent";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <main>
       <VoiceAgent />
+      <Analytics />
     </main>
   );
 }
